97e75a2c4557209f3050c4a80f95492e8fa4c840
sthandiwe3561 <sthandiwemsweli35@gmail.com>
2023-09-01 12:06:37 +0200

    added api information to the third carousel slide c

--- menu.js
+++ menu.js
@@ -9,14 +9,22 @@ function showUser(response) {
   let name = response.data.results[0].title;
   let card = document.querySelector("#card-text1");
   card.innerHTML = name;
-  let pic = document.querySelector("#picture-2");
-  pic.setAttribute(
+  let icon2 = document.querySelector("#picture-2");
+  icon2.setAttribute(
     "src",
     `https://spoonacular.com/recipeImages/649182-312x231.jpg`
   );
   let name2 = response.data.results[4].title;
   let card2 = document.querySelector("#card-text2");
   card2.innerHTML = name2;
+  let icon3 = document.querySelector("#picture-3");
+  icon3.setAttribute(
+    "src",
+    `https://spoonacular.com/recipeImages/641079-312x231.jpg`
+  );
+  let name3 = response.data.results[15].title;
+  let card3 = document.querySelector("#card-text3");
+  card3.innerHTML = name3;
 }
 
 let url = `https://api.spoonacular.com/recipes/complexSearch?query=lamb&number=20
