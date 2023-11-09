d957c14f9119747ec19161ba9dbdd80eeb69d3f5
sthandiwe3561 <sthandiwemsweli35@gmail.com>
2023-11-09 12:37:46 +0200

    added a starter file and images

--- menu.js
+++ menu.js
@@ -1,8 +1,1 @@
 alert("hello");
-function showUser(response) {
-  console.log(response);
-}
-
-let url = `https://api.spoonacular.com/recipes/complexSearch?query=meat&number=20
-&apiKey=4adddfff185f48fea173f8ec00939dfd`;
-axios.get(url).then(showUser);
