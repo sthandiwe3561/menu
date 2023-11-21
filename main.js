daa38cb4081a62bb93f84a185891771cfb20993e
sthandiwe3561 <sthandiwemsweli35@gmail.com>
2023-11-21 15:17:57 +0200

    modified files

--- main.js
+++ main.js
@@ -56,20 +56,3 @@ let foody = document.querySelector("#main");
 
 let main = document.querySelector("#menu");
 main.addEventListener("click", menu);
-
-function showInput(event) {
-  event.preventDefault();
-  let name = document.getElementById("text").value;
-  let date = document.getElementById("date").value;
-
-  if (name === "") {
-    alert("Please fill out the form");
-    return false;
-  } else {
-    let form = document.getElementById("response");
-    form.innerHTML = `Thank you ${name} we have recieved your request and your resevartion on ${date} is conformed`;
-  }
-}
-
-let ms = document.querySelector("#on");
-ms.addEventListener("click", showInput);
